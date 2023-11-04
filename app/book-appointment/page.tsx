4f631d5c62734147567095a5916fecb433513b87
Nice <hoangkimc93@gmail.com>
2023-11-04 11:16:10 +0700

    UI(form): init form

--- app/book-appointment/page.tsx
+++ app/book-appointment/page.tsx
@@ -1,8 +1,63 @@
-import React from 'react'
+"use client"
+import InputLink from '@/app/components/Input/InputLink'
+import { Col, Form, Row } from 'antd';
+import React, { Fragment } from 'react'
+import { useForm } from 'react-hook-form'
 
 const AppointmentForm = () => {
+  const {
+    control,
+    handleSubmit,
+    watch,
+    trigger,
+    clearErrors,
+    formState: { errors, dirtyFields, isDirty },
+  } = useForm({});
+
+  const [form] = Form.useForm();
   return (
-    <div>AppointmentForm</div>
+    <div className="max-w-7xl  mx-auto">
+      <div>
+
+      </div>
+      <div className='p-5'>
+        <Row gutter={32}>
+          <Col span={6}>
+            <InputLink placeholder="Họ và tên"
+              placement="top"
+              control={control}
+              name="name"
+              size="large">
+
+            </InputLink>
+          </Col>
+          <Col span={6}>
+            <InputLink placeholder="Giới tính"
+              placement="top"
+              control={control}
+              name="gender"
+              size="large">
+
+            </InputLink>
+          </Col>
+          <Col span={6}>
+            <InputLink placeholder="Số điện thoại"
+              placement="top"
+              control={control}
+              name="phone"
+              size="large">
+
+            </InputLink>
+          </Col>
+        </Row>
+        <InputLink placeholder="Ngày tháng năm sinh"
+          placement="top"
+          control={control}
+          name="birthday"
+          size="large">
+        </InputLink>
+      </div>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -21,8 +21,8 @@ export default async function Index() {
   const isSupabaseConnected = canInitSupabaseClient()
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      
+    <div className="flex-1 w-full flex flex-col gap-20 items-center ">
+
     </div>
   )
 }
