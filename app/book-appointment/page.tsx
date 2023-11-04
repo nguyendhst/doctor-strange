5b9866a76cd7e2f515c7d1d33245605a11bb0ac6
Nice <hoangkimc93@gmail.com>
2023-11-04 11:25:50 +0700

    chore(merge): merge

--- app/book-appointment/page.tsx
+++ app/book-appointment/page.tsx
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 "use client"
 import InputLink from '@/app/components/Input/InputLink'
 import { Col, Form, Row } from 'antd';
-import React, { Fragment } from 'react'
 import { useForm } from 'react-hook-form'
-=======
-'use client'
-import React, { useState } from 'react';
 import BookingSteps from '@/components/BookingSteps';
 
->>>>>>> bf926988af8dbd572e16d5cc8dd2c3ac7463c807
 
 const AppointmentForm = () => {
   const {
@@ -23,54 +17,52 @@ const AppointmentForm = () => {
 
   const [form] = Form.useForm();
   return (
-<<<<<<< HEAD
-    <div className="max-w-7xl  mx-auto">
-      <div>
+    <div className="max-w-7xl  mx-auto h-[calc(100vh-80px)] flex flex-col justify-center items-center">
+      <div className="bg-white h-fit p-5 w-full">
+        <BookingSteps>
+          <div className='p-5 flex flex-col gap-8'>
+            <Row gutter={32}>
+              <Col span={6}>
+                <InputLink placeholder="Họ và tên"
+                  placement="top"
+                  control={control}
+                  name="name"
+                  size="large">
 
+                </InputLink>
+              </Col>
+              <Col span={6}>
+                <InputLink placeholder="Giới tính"
+                  placement="top"
+                  control={control}
+                  name="gender"
+                  size="large">
+
+                </InputLink>
+              </Col>
+              <Col span={6}>
+                <InputLink placeholder="Số điện thoại"
+                  placement="top"
+                  control={control}
+                  name="phone"
+                  size="large">
+
+                </InputLink>
+              </Col>
+            </Row>
+            <Row gutter={32}>
+              <Col span={6}>
+                <InputLink placeholder="Ngày tháng năm sinh"
+                  placement="top"
+                  control={control}
+                  name="birthday"
+                  size="large">
+                </InputLink>
+              </Col>
+            </Row>
+          </div>
+        </BookingSteps>
       </div>
-      <div className='p-5'>
-        <Row gutter={32}>
-          <Col span={6}>
-            <InputLink placeholder="Họ và tên"
-              placement="top"
-              control={control}
-              name="name"
-              size="large">
-
-            </InputLink>
-          </Col>
-          <Col span={6}>
-            <InputLink placeholder="Giới tính"
-              placement="top"
-              control={control}
-              name="gender"
-              size="large">
-
-            </InputLink>
-          </Col>
-          <Col span={6}>
-            <InputLink placeholder="Số điện thoại"
-              placement="top"
-              control={control}
-              name="phone"
-              size="large">
-
-            </InputLink>
-          </Col>
-        </Row>
-        <InputLink placeholder="Ngày tháng năm sinh"
-          placement="top"
-          control={control}
-          name="birthday"
-          size="large">
-        </InputLink>
-      </div>
-=======
-    <div className="bg-white h-[100vh] p-5">
-      <BookingSteps>
-        
-      </BookingSteps>
->>>>>>> bf926988af8dbd572e16d5cc8dd2c3ac7463c807
     </div>
   )
 }
