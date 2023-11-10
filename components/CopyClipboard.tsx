42397d34af5f7077a01952465a9cc6b9a296aabf
caotrananhk <caotrananhk@gmail.com>
2023-11-11 00:05:13 +0700

    Update to merge

--- components/CopyClipboard.tsx
+++ components/CopyClipboard.tsx
@@ -1,17 +1,17 @@
-"use client";
+// "use client";
 
-import { LinkOutlined } from "@ant-design/icons";
+// import { LinkOutlined } from "@ant-design/icons";
 
-type AppointmentId = {
-  id: string;
-};
-export default function CopyClipboard({ id }: AppointmentId) {
-  const path = `/appointments#${id}`;
-  return (
-    <LinkOutlined
-      onClick={(e) =>
-        e.clipboardData.setData("text/plain", process.env.DOMAIN + path + "123")
-      }
-    />
-  );
-}
+// type AppointmentId = {
+//   id: string;
+// };
+// export default function CopyClipboard({ id }: AppointmentId) {
+//   const path = `/appointments#${id}`;
+//   return (
+//     <LinkOutlined
+//       onClick={(e) =>
+//         e.clipboardData.setData("text/plain", process.env.DOMAIN + path + "123")
+//       }
+//     />
+//   );
+// }
