625b2679794c0f5bb8e83dc977b9e0d68f0c433e
tona3922 <vothaitoan12@gmail.com>
2023-12-01 16:12:18 +0700

    chore: env key error

--- utils/supabase/findUser.ts
+++ utils/supabase/findUser.ts
@@ -1,8 +1,9 @@
 const { createClient } = require("@supabase/supabase-js");
 export const findUser = async (email: string) => {
   const supabase = await createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
   );
   const {
     data: { users },
