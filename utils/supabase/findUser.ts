69146071d5d87696d1ff0d4be1baf2e19ec33db5
nguyendhst <nguyendhst@proton.me>
2023-12-02 01:33:06 +0700

    test

--- utils/supabase/findUser.ts
+++ utils/supabase/findUser.ts
@@ -2,7 +2,7 @@ const { createClient } = require("@supabase/supabase-js");
 export const findUser = async (email: string) => {
   const supabase = await createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SERVICE_ROLE_KEY!,
     {
       auth: {
         autoRefreshToken: false,
