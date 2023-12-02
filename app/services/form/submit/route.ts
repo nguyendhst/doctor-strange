3412512fe84e2942fc2e5110267dbf1aba2be507
Nguyễn Nhật Nguyên <nguyendhst@proton.me>
2023-12-02 20:25:32 +0700

    Form Submit & UI Test (#28)
    * add more e2e
    
    * Auth form submisssion
    
    From Git Android
    
    * revert
    
    * revert route
    
    ---------
    
    Co-authored-by: mrdiamond12312 <102137001+mrdiamond12312@users.noreply.github.com>

--- app/services/form/submit/route.ts
+++ app/services/form/submit/route.ts
@@ -8,7 +8,19 @@ export async function POST(req: NextRequest): Promise<any> {
   const supabase = createClient(cookieStore)
 
   const json = await req.json();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
+  // Not Authorized
+  if (!user?.email)
+    return NextResponse.json({
+      statusCode: 403,
+      data: null,
+    }, {
+      status: 403,
+    });
+  
   const {
     data,
   } = await formSubmission(supabase, json);
