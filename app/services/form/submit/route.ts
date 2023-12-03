738152f75c823bbef350e55f88af478c0dc53f1e
mrdiamond12312 <102137001+mrdiamond12312@users.noreply.github.com>
2023-12-03 20:08:18 +0700

    merge dev (#32)
    * Add Doctor DTO
    
    * Feat: Doctor service
    
    * Add routes
    
    * Form Submit & UI Test (#28)
    
    * add more e2e
    
    * Auth form submisssion
    
    From Git Android
    
    * revert
    
    * revert route
    
    ---------
    
    Co-authored-by: mrdiamond12312 <102137001+mrdiamond12312@users.noreply.github.com>
    
    * Update get-appoint api and tests (#30)
    
    * Init
    
    * test(get-appointments): init first test by CTAK
    
    * Update test No data
    
    * Completed: E2E testing
    
    * Update authen for get-appoint api
    
    * Update get-appoint api
    
    * Update route.ts (401 >> 403)
    
    ---------
    
    Co-authored-by: caotrananhk <caotrananhk@gmail.com>
    Co-authored-by: Nice <hoangkimc93@gmail.com>
    
    * add schedule availability testing (#31)
    
    * Add Doctor DTO
    
    * Feat: Doctor service
    
    * Add routes
    
    * Add tests for displaying available shift options of a doctor at a specific date
    
    * Add testcase to query directly to real backend
    
    * Update test cases using decision table method
    
    ---------
    
    Co-authored-by: Minh Thuan <thuan17082002@gmail.com>
    Co-authored-by: Lephat902 <lehoangngocphat992002@gmail.com>
    
    * merge(dev): conflict solved
    
    ---------
    
    Co-authored-by: Minh Thuan <thuan17082002@gmail.com>
    Co-authored-by: Nguyễn Nhật Nguyên <nguyendhst@proton.me>
    Co-authored-by: CAO TRẦN ANH KHOA <108146451+caotrananhkhoa@users.noreply.github.com>
    Co-authored-by: caotrananhk <caotrananhk@gmail.com>
    Co-authored-by: Lephat902 <lehoangngocphat992002@gmail.com>

--- app/services/form/submit/route.ts
+++ app/services/form/submit/route.ts
@@ -1,39 +1,26 @@
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
-import { NextRequest, NextResponse } from 'next/server'
-import { formSubmission } from '@/services/form/form.service'
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { formSubmission } from "@/services/form/form.service";
+import { ServiceResponseDto } from "@/services/shared/dto/service-response.dto";
 
 export async function POST(req: NextRequest): Promise<any> {
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
 
-  // Get current user
+  const json = await req.json();
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
-  // Not Authorized, Haven't used ServiceResponseDto
+  // Not Authorized
   if (!user?.email)
-    return NextResponse.json({error: "Unauthorized"}, {
-      status: 403,
-    });
+    return NextResponse.json(
+      new ServiceResponseDto(403, null, "Unauthorized"),
+      {
+        status: 403,
+      }
+    );
 
-  const json = await req.json();
-
-  const {
-    data,
-  } = await formSubmission(supabase, json);
-
-  return data   
-  ? 
-  NextResponse.json({
-    statusCode: 201,
-    data: data,
-  })
-  :NextResponse.json( 
-  {
-    statusCode: 403,
-    data: data,
-  });
-
+  return NextResponse.json(await formSubmission(supabase, json));
 }
