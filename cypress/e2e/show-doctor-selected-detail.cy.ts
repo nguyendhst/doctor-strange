27c9c29238bd30e9a60530c6b1c5f0d6f50f42cd
trongphu012345@gmail.com <75671199+trongphuhappy@users.noreply.github.com>
2023-12-04 01:14:53 +0700

    feat(test): selected doctor detail

--- cypress/e2e/show-doctor-selected-detail.cy.ts
+++ cypress/e2e/show-doctor-selected-detail.cy.ts
@@ -1,5 +1,3 @@
-
-
 describe('Doctor Detail API', () => {
     it('should return doctor details by ID', () => {
         const doctorId = 2;
@@ -8,13 +6,29 @@ describe('Doctor Detail API', () => {
             method: 'GET',
             url: `/services/doctor/detail?id=${doctorId}`,
         }).then((response) => {
-            const doctorName = response.body.data.name;
-            expect(doctorName).to.not.be.empty;
-            
-            cy.showDoctorDetail(["Sprained ankle", "Hot flashes"], doctorName);
-
+            // Kiểm tra kết quả trả về từ API
             expect(response.status).to.eq(200);
             expect(response.body).to.have.property('data');
+
+            // Lấy thông tin bác sĩ từ API
+            const doctorData = response.body.data;
+
+            // Kiểm tra từng trường thông tin theo mẫu dữ liệu
+            expect(doctorData).to.include({
+            id: 2,
+            name: "Bob Williams",
+            specialization: "Orthopedist",
+            contact: "bob.williams@hospital.com",
+            rating: 4.7,
+            department: "Trung Vuong Hospital"
+            });
+
+            // Lấy tên bác sĩ từ dữ liệu API
+            const doctorName = doctorData.name;
+            expect(doctorName).to.not.be.empty;
+
+            // Gọi hàm showDoctorDetail với danh sách triệu chứng và tên bác sĩ
+            cy.showDoctorDetail(["Sprained ankle", "Hot flashes"], doctorName);
         });
     });
 });
