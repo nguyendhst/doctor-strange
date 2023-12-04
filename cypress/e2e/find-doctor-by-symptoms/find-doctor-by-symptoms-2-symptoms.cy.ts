7632651d2c4c4d9dc3528c492621c3a47aef095b
Nice <hoangkimc93@gmail.com>
2023-12-04 22:18:06 +0700

    test(search): additional test for searching doctors

--- cypress/e2e/find-doctor-by-symptoms/find-doctor-by-symptoms-2-symptoms.cy.ts
+++ cypress/e2e/find-doctor-by-symptoms/find-doctor-by-symptoms-2-symptoms.cy.ts
@@ -1,19 +1,49 @@
-
 describe("multi symptoms", () => {
   it("should return some doctors", () => {
     cy.step3FindDoctorBySymptom(["Sprained ankle", "Hot flashes"]);
-    cy.wait("@doctorBySymptoms").then((interception) => {
-      const responseBody = interception.response?.body;
 
-      const responseArrayLength = responseBody?.data?.length;
+    cy.get("@doctorBySymptoms.all") // this will get all intercepted requests
+      .invoke("at", -1) // intercept latest api call
+      .then((interception) => {
+        const responseBody = interception.response?.body;
 
-      cy.getAntdInputByLabel("Doctor Selection").click().wait(500);
-      cy.get(".ant-select-item.ant-select-item-option") // Select the doctor elements
-        .filter(":visible")
-        .its("length") // Get the count of elements from the HTML structure
-        .then((htmlCount) => {
-          expect(responseArrayLength).to.equal(htmlCount);
-        });
-    });
+        const responseArrayLength = responseBody?.data?.length;
+
+        cy.getAntdInputByLabel("Doctor Selection").click().wait(500);
+        cy.get(".ant-select-item.ant-select-item-option") // Select the doctor elements
+          .filter(":visible")
+          .its("length") // Get the count of elements from the HTML structure
+          .then((htmlCount) => {
+            expect(responseArrayLength).to.equal(htmlCount);
+          });
+      });
+  });
+  it("should return some doctors contain searchString", () => {
+    const SEARCH_STRING = "Bob";
+
+    cy.step3FindDoctorBySymptom(["Sprained ankle", "Hot flashes"]);
+
+    cy.getAntdInputByLabel("Doctor Selection").click().type(SEARCH_STRING);
+    cy.wait(500).wait("@doctorBySymptoms");
+
+    cy.get("@doctorBySymptoms.all") // this will get all intercepted requests
+      .invoke("at", -1) // intercept latest api call
+      .then((interception) => {
+        const responseBody = interception?.response?.body;
+
+        const responseArrayLength = responseBody?.data?.length;
+        
+        cy.getAntdInputByLabel("Doctor Selection").click().wait(500);
+        cy.get(".ant-select-item.ant-select-item-option") // Select the doctor elements
+          .filter(":visible")
+          .each((element) => {
+            const doctorName = element.find("div").text().trim();
+            expect(doctorName).contain(SEARCH_STRING);
+          })
+          .its("length") // Get the count of elements from the HTML structure
+          .then((htmlCount) => {
+            expect(responseArrayLength).to.equal(htmlCount);
+          });
+      });
   });
 });
